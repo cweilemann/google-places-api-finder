5bc11490da961b5d03579c463485646efbd3f887
Chris Weilemann <cweilemann@gmail.com>
2013-03-08 15:04:02 -0600

    + updates to how the next page tokens are stored + updates to *when* the next page tokens are stored

--- google_places_parser.rb
+++ google_places_parser.rb
@@ -9,13 +9,13 @@ class GooglePlacesParser
   ROOT_DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?"
 
   attr_accessor :address, :keywords, :radius
-  attr_reader :search_url, :original_search_results, :current_search_results, :next_page_tokens
+  attr_reader :search_url, :original_search_results, :current_search_results, :end_of_search_results, :next_page_tokens
 
   def initialize(args = nil)
     @api_key = API_KEY
     @search_url = ROOT_SEARCH_URL
     @radius = Settings.default_radius
-    @next_page_tokens = [nil]
+    @next_page_tokens = []
 
     @address, @keywords, @original_search_results, @current_search_results, @end_of_search_results = nil
 
@@ -33,23 +33,36 @@ class GooglePlacesParser
   end
 
   def get_next_page
-    return false if @end_of_search_results
+    return false if @end_of_search_results.present?
 
-    build_page_url
+    success = build_page_url(@next_page_tokens.last)
 
-    return false unless @search_url
-
-    execute
+    if success
+      execute
+    else
+      return "Error fetching next page..."
+    end
   end
 
   def get_previous_page
-    return @original_search_results if @next_page_tokens.last.nil?
+    @end_of_search_results = nil
+    @next_page_tokens.pop
+    @next_page_tokens.pop
 
-    build_page_url(@next_page_tokens.pop)
+    if @next_page_tokens.empty?
+      results_json = @original_search_results.present? ? @original_search_results : @current_search_results
+      store_results_info(results_json)
+      return get_place_names(results_json)
+    else
+      success = build_page_url(@next_page_tokens.last)
 
-    return false unless @search_url
+      if success
+        execute
+      else
+        return "Error fetching previous page..."
+      end
+    end
 
-    execute
   end
 
   def details(place_key)
@@ -72,14 +85,14 @@ class GooglePlacesParser
     append_types
   end
 
-  def build_page_url(page_token = nil)
-    return false if page_token == nil && @next_page_tokens.last.nil?
+  def build_page_url(next_page_token = nil)
+    return false if next_page_token == nil && @next_page_tokens.last.nil?
 
     reset_url
 
     append_default_parameters
 
-    append_next_page_token
+    append_next_page_token(next_page_token)
   end
 
   def build_details_url(place_key)
@@ -114,37 +127,43 @@ class GooglePlacesParser
     @search_url += "&types=#{Settings.default_type}"
   end
 
-  def append_next_page_token
-    @search_url += "&pagetoken=" + @next_page_tokens.last
+  def append_next_page_token(token)
+    @search_url += "&pagetoken=" + token
   end
 
   def sanitize_string(string)
     CGI.escape(string)
   end
 
-  def store_results_info(results_json)
-    next_page_token = begin
-                        results_json["next_page_token"]
-                        @end_of_search_results = nil
-                      rescue
-                        @end_of_search_results = results_json
-                        return
-                      end
-
-    @original_search_results = results_json if @original_search_results.nil?
-
-    @next_page_tokens.push(next_page_token) if next_page_token.present?
-
-    @current_search_results = results_json
+  def get_place_names(results_json)
+    results_json["results"].map {|result| result["name"]}
   end
 
-  def execute(for_search = true)
+  def store_results_info(results_json)
+    next_page_token = results_json["next_page_token"]
+
+    @original_search_results = results_json if @original_search_results.nil?
+    @end_of_search_results = results_json if next_page_token.nil?
+
+    @next_page_tokens.push(next_page_token)
+
+    #TODO: What should this return?
+  end
+
+  def execute(is_search = true)
     search_uri = URI.parse(URI.encode(@search_url))
 
     results_json = JSON.parse(open(search_uri).read)
 
-    store_results_info(results_json) if for_search
+    if is_search
+      @current_search_results = results_json
+      store_results_info(results_json)
+    end
 
-    results_json
+    if results_json.has_key?("status") && results_json["status"] == "OK"
+      return get_place_names(results_json)
+    else
+      return "Execution error: #{results_json["status"]}"
+    end
   end
 end
